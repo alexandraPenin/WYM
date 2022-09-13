419ba4caccde37919023fbc52b690981500d8980
alexandraPenin <alexandra.cecile.penin@gmail.fr>
2022-09-13 16:14:10 +0200

    Modification des fichiers models et app par Alex

--- wym-project/wym_project/app.py
+++ wym-project/wym_project/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from model_utils import resume_text
 
 app = Flask(__name__)
 
@@ -8,4 +8,31 @@ def home():
 	return render_template('index.html')
 
 
-app.run(debug=True, host='0.0.0.0')
+#app.run(debug=True, host='0.0.0.0')
+
+@app.route("/about") 
+def about():
+    return render_template("about.html") 
+
+@app.route("/model",methods=["GET","POST"]) 
+def model():
+    if request.method=="GET":
+        return render_template("model.html")
+    elif request.method=="POST":
+        form_data_text=request.form['textmodel']
+        form_data_resum=resume_text(form_data_text)
+        print(form_data_resum)
+    return render_template("model.html",resume=form_data_resum)
+
+	
+@app.route("/contact") 
+def contact():
+    return render_template("contact.html") 
+
+@app.route("/contacted") 
+def contacted():
+    return render_template("contacted.html") 
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host="0.0.0.0", port=5001) # démarrage de l’application
--- wym-project/wym_project/model_utils.py
+++ wym-project/wym_project/model_utils.py
@@ -3,8 +3,15 @@
 # importing the requests library
 import requests
 
+def resume_text(texte):
+#fonction qui récupère une chaîne de caractère et qui affiche un résumé de cette fonction
+    payload = {"text":[texte]}
+    r = requests.post(url = 'http://0.0.0.0:5000/model/predict', json = payload)
+    return r.json()
 
-payload = {"text":["MON TEXTE A RESUMER EST"]}
-r = requests.post(url = 'http://0.0.0.0:5000/model/predict', json = payload)
-print(r.url)
-print(r.json())
+
+#payload = {"text":["MON TEXTE A RESUMER EST"]}
+#test_fonct1= resume_text("MON TEXTE A RESUMER EST")
+#print(test_fonct1)
+
+#fonction qui prend le texte en param et qui renvoie le résumer
