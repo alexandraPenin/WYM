89a8273ee5ad4b2f2a5b961724cc523a22259d61
samahAlradi <samah.al-radi@le-campus-numerique.fr>
2022-09-14 16:24:01 +0200

    integrer la page model d'Alexandra sur la page web et créer un fichier docker compose

--- wym-project/wym_project/app.py
+++ wym-project/wym_project/app.py
@@ -3,14 +3,24 @@ from model_utils import resume_text
 import psycopg2
 import database
 import db_utils
+import os
+
 from db_utils import User, pgdb
 
+if os.getenv("DOCKER_BUILD")==1 :
+    db_host = os.getenv("DB_HOST")
+    model_host = os.getenv("MODEL_HOST")
+else:
+    db_host = "localhost"
+    model_host ="localhost"
+
 app = Flask(__name__)
-mydb = pgdb("localhost",5432)
+mydb = pgdb(db_host,5432)
 
 @app.route("/")
 def home():
 	return render_template('index.html')
+
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
@@ -32,19 +42,23 @@ def form():
 def about():
     return render_template("about.html") 
 
+
+
 @app.route("/model",methods=["GET","POST"]) 
 def model():
     if request.method=="GET":
         return render_template("model.html")
     elif request.method=="POST":
         form_data_text=request.form['textmodel']
-        form_data_resum=resume_text(form_data_text)
+        form_data_resum=resume_text(form_data_text,model_host=model_host)
         print(form_data_resum)
-    return render_template("model.html",resume=form_data_resum)
+    return render_template("model.html", resume=form_data_resum['summary_text'][0])
+    
 	
 @app.route("/contact") 
 def contact():
     return render_template("contact.html") 
+
 
 @app.route("/contacted") 
 def contacted():
